a22183946fa37ccc8f79eea5c5fe9492a60e705e
iamhi <inpactt@gmail.com>
2023-01-12 19:44:17 +0100

    Adding HTTPS server to sswagger

--- src/main/java/com/github/iamhi/hizone/authentication/v2/config/SwaggerSettingsConfig.java
+++ src/main/java/com/github/iamhi/hizone/authentication/v2/config/SwaggerSettingsConfig.java
@@ -4,10 +4,21 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Hi-Zone Authentication-v2 api", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Hi-Zone Authentication-v2 api", version = "v1"),
+    servers = {
+        @Server(
+            url = "https://api.ibeenhi.com/hi-zone-api/authentication-v2",
+            description = "Https variant"
+        ),
+        @Server(
+            url = "http://api.ibeenhi.com/hi-zone-api/authentication-v2",
+            description = "Http variant"
+        )
+    })
 @SecurityScheme(
     name = SwaggerSettingsConfig.AUTHENTICATION_SCHEME_NAME,
     type = SecuritySchemeType.HTTP,
